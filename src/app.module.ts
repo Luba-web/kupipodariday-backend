e58d5eeed16a3d2d9c08ce679ef0a10706e7ed86
Luba-web <ldvasileva87@yandex.ru>
2023-02-19 22:48:20 +0300

    init project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { OffersModule } from './offers/offers.module';
+import { WishesModule } from './wishes/wishes.module';
+import { WishlistsModule } from './wishlists/wishlists.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'student',
+      password: 'student',
+      database: 'kupipodariday',
+      entities: [],
+      synchronize: true,
+    }),
+    UsersModule,
+    OffersModule,
+    WishesModule,
+    WishlistsModule,
+  ],
   controllers: [AppController],
   providers: [],
 })
